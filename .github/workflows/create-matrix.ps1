753b8bf21c5df88f1c4e758fe1eb1dc86a300b28
Antonio Bravo <bravo.antonio@gmail.com>
2022-09-19 20:03:53 +0200

    Initial Commit

---
+++ .github/workflows/create-matrix.ps1
@@ -0,0 +1,23 @@
+param(
+#   [String]$element = "db",
+#   [String[]]$array,
+  [Switch]$Raw 
+)
+
+$Raw = $false
+# $element = "db"
+[String[]]$array = ("db1", "db2")
+$elements = @()
+
+@($array) | ForEach-Object {
+    $elements += @{
+        db = $_
+    }
+}
+
+if ($Raw) {
+    Write-Host ($elements | ConvertTo-JSON)
+  } else {
+    # Output the result for consumption by GH Actions
+    Write-Host "::set-output name=matrix::$($elements | ConvertTo-JSON -Compress)"
+  }
