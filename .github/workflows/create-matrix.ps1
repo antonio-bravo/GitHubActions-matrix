c6c8601fc61af174febc87518e378c39782c2ee3
antonio-bravo <79697559+antonio-bravo@users.noreply.github.com>
2022-09-20 12:42:25 +0200

    change pwsh function

--- .github/workflows/create-matrix.ps1
+++ .github/workflows/create-matrix.ps1
@@ -1,35 +1,19 @@
-# param(
-# #   [String]$element = "db",
-# #   [String[]]$array,
-#   [Switch]$Raw 
-# )
+param(
+  [String]$element = "db",
+#   [String[]]$array,
+)
 
-# $Raw = $false
-# # $element = "db"
-# [String[]]$array = ('db1', 'db2')
-# $elements = @()
+$Raw = $false
+# $element = "db"
+[String[]]$array = ('db1', 'db2')
+$elements = @()
 
-# @($array) | ForEach-Object {
-#     $elements += @{
-#         db = $_
-#     }
-# }
-
-#  if ($Raw) {
-#     Write-Host (($elements | ConvertTo-JSON -Compress) -replace '([\\]*)"', '$1$1\"'))
-#   } else {
-#     # Output the result for consumption by GH Actions
-#     #Write-Host "::set-output name=matrix::$($($elements | ConvertTo-JSON -Compress) -replace '([\\]*)"', '$1$1\"'))"
-#      Write-Host ($($elements | ConvertTo-JSON -Compress) -replace '([\\]*)"', '$1$1\"')
-     
-#   }
-
-$Jobs = @()
-
-@('ubuntu-latest', 'windows-latest') | ForEach-Object {
-  $Jobs += @{
-    db = $_
-  }
+@($array) | ForEach-Object {
+    $elements += @{
+        db = $_
+    }
 }
-return $($Jobs | ConvertTo-JSON -Compress)
-#Write-Host "::set-output name=matrix::$($Jobs | ConvertTo-JSON -Compress))"
+
+# Output the result for consumption by GH Actions
+#Write-Host "::set-output name=matrix::$($($elements | ConvertTo-JSON -Compress) -replace '([\\]*)"', '$1$1\"'))"
+return $($elements | ConvertTo-JSON -Compress)
