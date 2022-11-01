31896119265a4b77aa0306c7fcb5461afc90dcb2
antonio-bravo <79697559+antonio-bravo@users.noreply.github.com>
2022-11-01 14:22:46 +0100

    handle remove [] when there is more than 1 element

--- .github/workflows/create-matrix.ps1
+++ .github/workflows/create-matrix.ps1
@@ -6,7 +6,7 @@ param(
 # $element = "db"
 # [String[]]$array = ('db1', 'db2')
 $elements = @()
-#$countarray = $array.Count
+$countarray = $array.Count
 
 @($array) | ForEach-Object {
     $elements += @{
@@ -16,11 +16,11 @@ $elements = @()
 
 # Output the result for consumption by GH Actions
 #Write-Host "::set-output name=matrix::$($($elements | ConvertTo-JSON -Compress) -replace '([\\]*)"', '$1$1\"'))"
-#if ($countarray = 1) {
+if ($countarray = 1) {
   #add [] when there is only one element in the array
-  return $($elements | ConvertTo-JSON -Compress )
-# }
-# else {
-#   return $($elements | ConvertTo-JSON -Compress)
-# }
+  return "[$($elements | ConvertTo-JSON -Compress )]"
+ }
+ else {
+   return $($elements | ConvertTo-JSON -Compress)
+ }
 
